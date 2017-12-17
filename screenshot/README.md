bab17ad3f7d359e57668fabfb4e44a72bb4b59b0
Thomas Sileo <t@a4.io>
2017-12-17 17:15:44 +0100

    Add screenshot script

---
+++ screenshot/README.md
@@ -0,0 +1,17 @@
+# screenshot 
+
+Take full-page screenshot via CLI using [Puppeteer](https://github.com/GoogleChrome/puppeteer)/Headless Chrome.
+
+## Setup
+
+Node and yarn are required.
+
+```
+$ yarn install
+```
+
+## Usage
+
+```
+$ node sc.js http://duckduckgo.com/ ddg.png
+```
---
+++ screenshot/sc.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+const url = process.argv[2];
+const o = process.argv[3];
+
+if (o == undefined || url == undefined) {
+    console.log('sc.js <url> <output file>');
+    process.exit(2);
+}
+
+async function run() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  page.setViewport({width: 1280, height: 800});
+
+  await page.goto(url);
+  await page.screenshot({ path: o, fullPage: true });
+
+  browser.close();
+}
+
+run();
